fec57207480c6df752691d2d03e7ed55c485a2fb
TalLazar <tal@quonder.com>
2024-09-20 14:06:07 -0400

    Update onRequestPost.js

--- functions/onRequestPost.js
+++ functions/onRequestPost.js
@@ -1,11 +1,9 @@
 
 
-export async function onRequestPost(context) {
-    try {
-        console.log("here");
-    } catch (err) {
-        console.log("here again");
-    }
-
-    return;
-  }
+async function onRequestPost(context) {
+    return await fetch("https://example.com", {
+        headers: {
+          "X-Source": "Cloudflare-Workers",
+        },
+      });
+}
